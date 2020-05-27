e69d97353fde473034816955b7e992763b35e9ba
Alex Eftimie <alex.eftimie@getyourguide.com>
2020-05-27 19:05:04 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -5,5 +5,10 @@
 #
 #ln -s /usr/local/bin /usr/local/lib/python2.7/bin
 
+rm -r dist/
 export SENTRY_LIGHT_BUILD=1
 python setup.py bdist
+python setup.py bdist_wheel
+
+echo "Done. Check the dist/ folder for the output"
+
